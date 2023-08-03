ab0f6b40a99bd5f652200fefde50ea7eb2001d15
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-08-03 17:19:05 +0200

    Made adaptive for main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,30 @@
 import '../scss/main.scss'
 
+// Функционал для меню
+const nav = document.querySelector('#nav')
+const navBtn = document.querySelector('#nav-btn')
+const navBtnImg = document.querySelector('#nav-btn-img')
+
+navBtn.onclick = () => {
+	if (nav.classList.toggle('open')) {
+		navBtnImg.src = '/public/images/x-btn.svg'
+	} else {
+		navBtnImg.src = '/public/images/nav-button.svg'
+	}
+}
+
 gsap.registerPlugin(ScrollTrigger)
 
 const tl = gsap.timeline()
 tl.to('.wrapper', 4, { x: -window.innerWidth * 1.2 })
 
 ScrollTrigger.create({
-  animation: tl,
-  trigger: '.wrapper',
-  start: 'center center',
-  end: '+=3000',
-  scrub: true,
-  pin: true,
+	animation: tl,
+	trigger: '.wrapper',
+	start: 'center center',
+	end: '+=3000',
+	scrub: true,
+	pin: true,
 })
 
 // Анимация для карточек
@@ -23,34 +36,34 @@ const header = document.querySelector('.thirdSection__heading')
 const animation = gsap.timeline()
 
 cards.forEach((card, index) => {
-  if (index > 0) {
-    gsap.set(card, { y: index * 600 })
-    animation.to(card, { y: 0, duration: index * 0.5, ease: 'none' }, 0)
-  }
+	if (index > 0) {
+		gsap.set(card, { y: index * 600 })
+		animation.to(card, { y: 0, duration: index * 0.5, ease: 'none' }, 0)
+	}
 })
 
 ScrollTrigger.create({
-  trigger: '.wrap',
-  start: 'top top',
-  pin: true,
-  end: `+=${cards.length * 400}`,
-  scrub: true,
-  animation: animation,
-  // markers: true,
+	trigger: '.wrap',
+	start: 'top top',
+	pin: true,
+	end: `+=${cards.length * 400}`,
+	scrub: true,
+	animation: animation,
+	// markers: true,
 })
 
 // HOVER ДЛЯ КЕЙСОВ
 const addHoverEffect = (hoverElementSelector, textElementSelector) => {
-  const hoverElement = document.querySelector(hoverElementSelector)
-  const textElement = document.querySelector(textElementSelector)
+	const hoverElement = document.querySelector(hoverElementSelector)
+	const textElement = document.querySelector(textElementSelector)
 
-  hoverElement.addEventListener('mouseover', () => {
-    textElement.style.opacity = '1'
-  })
+	hoverElement.addEventListener('mouseover', () => {
+		textElement.style.opacity = '1'
+	})
 
-  hoverElement.addEventListener('mouseout', () => {
-    textElement.style.opacity = '0'
-  })
+	hoverElement.addEventListener('mouseout', () => {
+		textElement.style.opacity = '0'
+	})
 }
 
 const caseText1 = document.querySelector('.case1__hover')
@@ -80,92 +93,92 @@ let footerLink = document.querySelector('.footer-link')
 
 gsap.registerPlugin(ScrollTrigger)
 const tl2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: blue,
-    scrub: 1,
-    start: 'top center+=200',
-    end: 'top center-=200',
-    // markers: true,
-  },
+	scrollTrigger: {
+		trigger: blue,
+		scrub: 1,
+		start: 'top center+=200',
+		end: 'top center-=200',
+		// markers: true,
+	},
 })
 
 tl2.fromTo(blue, { y: -100, duration: 5 }, { y: 350, duration: 5 }).to(blue, {
-  y: -100,
-  duration: 5,
+	y: -100,
+	duration: 5,
 })
 
 // Картинка
 const tl3 = gsap.timeline({
-  scrollTrigger: {
-    trigger: blue,
-    scrub: 1,
-    start: 'top center+=200',
-    end: 'top center-=200',
-    // markers: true,
-  },
+	scrollTrigger: {
+		trigger: blue,
+		scrub: 1,
+		start: 'top center+=200',
+		end: 'top center-=200',
+		// markers: true,
+	},
 })
 
 tl3
-  .fromTo(
-    img,
-    { y: 200, duration: 5, scale: 0.7, filter: 'blur(5px)' },
-    { y: -100, duration: 5, filter: 'blur(0)' }
-  )
-  .to(img, { y: -100, duration: 5, zIndex: 10, scale: 1 })
-  .to(img, { y: 200, duration: 10 })
+	.fromTo(
+		img,
+		{ y: 200, duration: 5, scale: 0.7, filter: 'blur(5px)' },
+		{ y: -100, duration: 5, filter: 'blur(0)' }
+	)
+	.to(img, { y: -100, duration: 5, zIndex: 10, scale: 1 })
+	.to(img, { y: 200, duration: 10 })
 
 // Изменение черного цвета footer
 const tl4 = gsap.timeline({
-  scrollTrigger: {
-    trigger: blue,
-    scrub: 1,
-    start: 'top center',
-    end: 'top center-=200',
-    // markers: true,
-  },
+	scrollTrigger: {
+		trigger: blue,
+		scrub: 1,
+		start: 'top center',
+		end: 'top center-=200',
+		// markers: true,
+	},
 })
 
 tl4.to(footer, { background: '#2BE2F0', duration: 2 })
 
 // Фиксация текста 1
 const tl5 = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.text1',
-    scrub: 1,
-    start: 'top top',
-    end: '+=300',
-    pin: true,
-    pinSpacing: false,
-    // markers: true,
-  },
+	scrollTrigger: {
+		trigger: '.text1',
+		scrub: 1,
+		start: 'top top',
+		end: '+=300',
+		pin: true,
+		pinSpacing: false,
+		// markers: true,
+	},
 })
 
 tl5.to('.text1', {})
 
 // Фиксация текста 2
 const tl6 = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.text2',
-    scrub: 1,
-    start: 'top-=160 top',
-    end: '+=300',
-    pin: true,
-    pinSpacing: false,
-    // markers: true,
-  },
+	scrollTrigger: {
+		trigger: '.text2',
+		scrub: 1,
+		start: 'top-=160 top',
+		end: '+=300',
+		pin: true,
+		pinSpacing: false,
+		// markers: true,
+	},
 })
 
 tl6.to('.text2', {})
 
 // Появление Ссылки
 const tl7 = gsap.timeline({
-  scrollTrigger: {
-    trigger: blue,
-    scrub: 1,
-    start: 'top center-=200',
-    end: 'top center-=300',
-    // markers: true,
-  },
+	scrollTrigger: {
+		trigger: blue,
+		scrub: 1,
+		start: 'top center-=200',
+		end: 'top center-=300',
+		// markers: true,
+	},
 })
 
 tl7.to(footerLink, { opacity: 1, delay: 1 })
